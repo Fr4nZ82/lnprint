0f578f15ba26fe030f67a815ebf2a775e1465af1
Fr4nZ82 <35651871+Fr4nZ82@users.noreply.github.com>
2019-07-04 20:18:56 +0200

    removed unused scripts

--- my-ln-service/push/index.js
+++
@@ -1,6 +0,0 @@
-const subscribeToInvoices = require('./invoices');
-const subscribeToTransactions = require('./transactions');
-const verifyClient = require('./verify_client');
-
-module.exports = {subscribeToInvoices, subscribeToTransactions, verifyClient};
-
